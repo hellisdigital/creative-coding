ab1b1d01513600411655b3ae0bdcc64274a8c805
Przene <padol19@gmail.com>
2022-03-22 23:15:54 +0100

    feat: copied intro.html loop to sketch-01.js

--- sketches/sketch-01.js
+++ sketches/sketch-01.js
@@ -1,13 +1,37 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: [ 600, 600 ]
 };
 
 const sketch = () => {
   return ({ context, width, height }) => {
+
+    const w = 60;
+    const h = 60;
+    const gap = 25;
+    let x, y;
+
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+
+
+    for (let i = 0; i < 5; i++) {
+      for (let j = 0; j < 5; j++) {
+        x = 100 + (w + gap) * i;
+        y = 100 + (h + gap) * j;
+        context.beginPath();
+        context.rect(x, y, w, h);
+        context.stroke();
+
+        if (Math.random() > 0.5) {
+          context.beginPath();
+          context.rect(x + 8, y + 8, w, h);
+          context.stroke();
+        }
+      }
+    }
+
   };
 };
 

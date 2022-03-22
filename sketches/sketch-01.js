4ebd00d03eec4b2e30c070a79887f74abd9ab546
Przene <padol19@gmail.com>
2022-03-22 23:27:08 +0100

    feat: dynamic values for sketch

--- sketches/sketch-01.js
+++ sketches/sketch-01.js
@@ -1,32 +1,40 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 600, 600 ]
+  dimensions: [1080, 1080]
+  //dimensions: 'A4',
+  //pixelsPerInch: 300,
+  //orientation: 'landscape'
 };
 
 const sketch = () => {
   return ({ context, width, height }) => {
 
-    const w = 60;
-    const h = 60;
-    const gap = 25;
-    let x, y;
-
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+    context.lineWidth = width * 0.005;
 
+    const w = width * 0.10;
+    const h = height * 0.10;
+    const gap = width * 0.03;
+    const inital_x = width * 0.17;
+    const inital_y = height * 0.17;
+
+    const off = width * 0.02;
+
+    let x, y;
 
     for (let i = 0; i < 5; i++) {
       for (let j = 0; j < 5; j++) {
-        x = 100 + (w + gap) * i;
-        y = 100 + (h + gap) * j;
+        x = inital_x + (w + gap) * i;
+        y = inital_y + (h + gap) * j;
         context.beginPath();
         context.rect(x, y, w, h);
         context.stroke();
 
         if (Math.random() > 0.5) {
           context.beginPath();
-          context.rect(x + 8, y + 8, w, h);
+          context.rect(x + off/2, y + off/2, w - off, h - off);
           context.stroke();
         }
       }

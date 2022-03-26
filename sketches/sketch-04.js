f3b5a487afa83e8987c457279bb19cf72a4653e5
Przene <padol19@gmail.com>
2022-03-26 23:57:02 +0100

    feat: more points

--- sketches/sketch-04.js
+++ sketches/sketch-04.js
@@ -13,14 +13,12 @@ const sketch = () => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    const point = {
-      x: 800,
-      y: 400,
-      radius: 10
-    };
+    const pointA = new Point(800, 400, 10);
+    const pointB = new Point(600, 200, 20);
 
     context.beginPath();
-    context.arc(point.x, point.y, point.radius, 0, Math.PI * 2);
+    context.arc(pointA.x, pointA.y, pointA.radius, 0, Math.PI * 2);
+    context.arc(pointB.x, pointB.y, pointB.radius, 0, Math.PI * 2);
     context.fillStyle = "#000";
     context.fill();
   };

68d8c3c1eb93fd1510e191257ec4e0f413105d7b
Przene <padol19@gmail.com>
2022-04-02 23:01:44 +0200

    feat: drawing our own shapes based on letter

--- sketches/sketch-06.js
+++ sketches/sketch-06.js
@@ -14,18 +14,32 @@ let fontSize = 1200;
 let fontFamily = "VCRmono";
 //fontFamily = 'serif'
 
-const sketch = () => {
-  return ({ context, width, height }) => {
-    context.fillStyle = "white";
-    context.fillRect(0, 0, width, height);
+const typeCanvas = document.createElement("canvas");
+const typeContext = typeCanvas.getContext("2d");
 
-    context.fillStyle = "#000";
-    context.font = `${fontSize}px ${fontFamily}`;
-    context.textBaseline = "top";
+const sketch = ({ context, width, height }) => {
+  const cell = 20;
+  const numCols = Math.floor(width / cell);
+  const numRows = Math.floor(height / cell);
+
+  const numCells = numCols * numRows;
+
+  typeCanvas.width = numCols;
+  typeCanvas.height = numRows;
+
+  return ({ context, width, height }) => {
+    typeContext.fillStyle = "black";
+    typeContext.fillRect(0, 0, numCols, numRows);
+
+    fontSize = numCols;
+
+    typeContext.fillStyle = "white";
+    typeContext.font = `${fontSize}px ${fontFamily}`;
+    typeContext.textBaseline = "top";
     //context.textAlign = "center";
 
-    const metrics = context.measureText(text);
-    console.log(metrics);
+    const metrics = typeContext.measureText(text);
+    //console.log(metrics);
 
     const mx = metrics.actualBoundingBoxLeft * -1;
     const my = metrics.actualBoundingBoxAscent * -1;
@@ -33,17 +47,47 @@ const sketch = () => {
     const mh =
       metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
 
-    const tx = (width - mw) / 2 - mx;
-    const ty = (height - mh) / 2 - my;
+    const tx = (numCols - mw) / 2 - mx;
+    const ty = (numRows - mh) / 2 - my;
 
-    context.save();
-    context.translate(tx, ty);
+    typeContext.save();
+    typeContext.translate(tx, ty);
 
-    context.beginPath();
-    context.rect(mx, my, mw, mh);
-    context.stroke();
-    context.fillText(text, 0, 0);
-    context.restore();
+    typeContext.beginPath();
+    typeContext.rect(mx, my, mw, mh);
+    typeContext.stroke();
+
+    typeContext.fillText(text, 0, 0);
+    typeContext.restore();
+
+    const typeData = typeContext.getImageData(0, 0, numCols, numRows).data;
+
+    //context.drawImage(typeCanvas, 0, 0);
+
+    for (let i = 0; i < numCells; i++) {
+      const col = i % numCols;
+      const row = Math.floor(i / numCols);
+
+      const x = col * cell;
+      const y = row * cell;
+
+      const r = typeData[i * 4 + 0];
+      const g = typeData[i * 4 + 1];
+      const b = typeData[i * 4 + 2];
+
+      context.fillStyle = `rgb(${r},${g},${b})`;
+      context.save();
+      context.translate(x, y);
+      context.translate(cell / 2, cell / 2);
+      //context.fillRect(0,0,cell,cell)
+
+      context.beginPath();
+      context.arc(0, 0, cell / 2, 0, Math.PI * 2);
+      context.fill();
+
+      context.restore();
+      //console.log(typeData)
+    }
   };
 };
 
